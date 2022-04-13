104eb385d4a6176f71234f17a385bb5c7b81e156
yx-hyperion <97842923+yx-hyperion@users.noreply.github.com>
2022-04-13 11:44:05 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'c80e7a33ced84d5b8d2e432d345b7c0c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '青椒小熊猫',
+  domain: 'https://econyu.notion.site/c80e7a33ced84d5b8d2e432d345b7c0c',
+  author: 'Dr.Hyperion',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
